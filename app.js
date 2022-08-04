6d1c744c9e09577178c0c655a589f02b3575f25f
Seo Ho Yi <seohoyi@10-248-222-64.wireless.oregonstate.edu>
2022-08-04 09:37:15 -0700

    day 1 progress

---
+++ app.js
@@ -0,0 +1,24 @@
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controlls');
+const sectBtn = document.querySelectorAll('.control');
+const allSections = document.querySelectorAll('.main-content');
+
+
+function PageTransitions(){
+    //Button click active class
+    for(let i =0; i < sectBtn.length; i++){
+        // don't use arrow functions('=>') when using 'this' 
+        // keywords it doesn't work
+        sectBtn[i].addEventListener('click', function(){
+            let currentBtn = document.querySelectorAll('.active-btn');
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+            this.className += 'active-btn'
+        })
+    }
+    
+    //sections Active Class
+    allSections.addEventListener('click', (e) =>{
+        console.log(e.target);
+    })
+}
+PageTransitions();
